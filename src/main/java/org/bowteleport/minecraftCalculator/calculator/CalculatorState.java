d7763becccb42b5763d6775e72ab1a5a2f570806
skbaby <130176463+skbabyreal@users.noreply.github.com>
2026-09-08 08:58:25 -0700

    Add src/main/java/org/bowteleport/minecraftCalculator/calculator/CalculatorState.java

---
+++ src/main/java/org/bowteleport/minecraftCalculator/calculator/CalculatorState.java
@@ -0,0 +1,17 @@
+package org.bowteleport.minecraftCalculator.calculator;
+
+
+public enum CalculatorState {
+
+    EMPTY,
+
+    INPUT_FIRST_NUMBER,
+
+    OPERATION_SELECTED,
+
+    INPUT_SECOND_NUMBER,
+
+    RESULT,
+
+    ERROR
+}
